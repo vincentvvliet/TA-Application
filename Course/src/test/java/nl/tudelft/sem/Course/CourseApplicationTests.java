45f8303a9b845db6b7411506ad53b0c78d30a36c
Iarina Tudor <i.m.tudor@student.tudelft.nl>
2021-12-01 19:35:32 +0100

    Add Course module

---
+++ Course/src/test/java/nl/tudelft/sem/Course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem.Course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
