5016af21073c84fbf161277724c70019d51ee232
Iarina Tudor <i.m.tudor@student.tudelft.nl>
2021-12-01 19:40:24 +0100

    Add User module

---
+++ User/src/test/java/nl/tudelft/sem/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
