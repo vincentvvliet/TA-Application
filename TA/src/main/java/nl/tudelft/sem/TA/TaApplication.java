6ee16d11a11886200e848634dc5d1d2aa3c5c679
Iarina Tudor <i.m.tudor@student.tudelft.nl>
2021-12-01 19:17:38 +0100

    Added new TA module

---
+++ TA/src/main/java/nl/tudelft/sem/TA/TaApplication.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem.TA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaApplication.class, args);
+	}
+
+}
---
+++ TA/src/test/java/nl/tudelft/sem/TA/TaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem.TA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
