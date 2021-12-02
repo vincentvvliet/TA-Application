fcfb01d5b59abeb070a6d5118b0cc1101727cdef
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2021-12-02 11:56:31 +0100

    Fixed checkstyle

--- TAs/src/main/java/nl/tudelft/sem/TAs/TAsApplication.java
+++ TAs/src/main/java/nl/tudelft/sem/TAs/TAsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TAsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TAsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TAsApplication.class, args);
+    }
 
 }
