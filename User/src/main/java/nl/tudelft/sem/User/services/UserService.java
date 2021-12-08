abe2c9dea28539c1b8b23a5ae7d77864c29a1b42
Andrei Tociu <A.C.Tociu@student.tudelft.nl>
2021-12-08 15:47:25 +0100

    add service in user to accept TA application

---
+++ User/src/main/java/nl/tudelft/sem/User/services/UserService.java
@@ -0,0 +1,34 @@
+package nl.tudelft.sem.User.services;
+
+import nl.tudelft.sem.User.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * accepts the specified TA application
+     * @param applicationId of the TA application to be accepted
+     * @return true if application successfully accepted, false otherwise
+     */
+    public boolean selectTaApplication(UUID applicationId) {
+        WebClient client = WebClient.create();
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.method(HttpMethod.POST);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(URI.create("localhost:47112/application/acceptApplication/" + applicationId ));
+        Mono<Boolean> response = bodySpec.retrieve().bodyToMono(Boolean.class);
+        Optional<Boolean> result = response.blockOptional(Duration.of(1000, ChronoUnit.MILLIS));
+        return result.orElse(false);
+    }
+}
