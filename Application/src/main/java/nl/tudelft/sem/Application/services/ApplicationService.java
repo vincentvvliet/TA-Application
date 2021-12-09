ff87b39ad9603d8e84f3e0f62ad8811ac07153d7
Gissa <G.vandeLinde@student.tudelft.nl>
2021-12-09 09:52:23 +0100

    Fix Checkstyle complaints

--- Application/src/main/java/nl/tudelft/sem/Application/services/ApplicationService.java
+++ Application/src/main/java/nl/tudelft/sem/Application/services/ApplicationService.java
@@ -1,5 +1,13 @@
 package nl.tudelft.sem.Application.services;
 
+import java.net.URI;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import javax.management.InvalidApplicationException;
 import nl.tudelft.sem.Application.entities.Application;
 import nl.tudelft.sem.Application.repositories.ApplicationRepository;
 import nl.tudelft.sem.Application.services.validator.IsCourseOpen;
@@ -14,14 +22,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import javax.management.InvalidApplicationException;
-import java.net.URI;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class ApplicationService {
@@ -36,59 +36,65 @@ public class ApplicationService {
     }
 
     /**
-     * check if the ration of 1 TA for every 20 students is already met
-     * @return true is ratio is already met, false otherwise
+     * Check if the ration of 1 TA for every 20 students is already met.
+     *
+     * @return true is ratio is already met, false otherwise.
      */
     public boolean isTASpotAvailable(UUID courseId) {
         return true;
     }
 
     /**
-     * creates a new TA once an application has been accepted
-     * @param studentId of the student that becomes TA
-     * @param courseId of the course for which student is TA
-     * @return true if the TA was successfully created
+     * Creates a new TA once an application has been accepted.
+     *
+     * @param studentId of the student that becomes TA.
+     * @param courseId of the course for which student is TA.
+     *
+     * @return true if the TA was successfully created.
      */
     public boolean createTA(UUID studentId, UUID courseId) {
         WebClient client = WebClient.create();
         WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.method(HttpMethod.POST);
-        WebClient.RequestBodySpec bodySpec = uriSpec.uri(URI.create("localhost:47112/TA/createTA/" + studentId  + "/" + courseId));
+        URI uri = URI.create("localhost:47112/TA/createTA/" + studentId  + "/" + courseId);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uri);
         Mono<Boolean> response = bodySpec.retrieve().bodyToMono(Boolean.class);
         Optional<Boolean> result = response.blockOptional(Duration.of(1000, ChronoUnit.MILLIS));
         return result.orElse(false);
     }
 
-    /** Ask the Course microservice for the grade corresponding to the student and course ID of the application
+    /** Ask the Course microservice for the grade corresponding to
+     * the student and course ID of the application.
      *
      * @return A Optional double
      */
-    public Optional<Double> getGrade(UUID studentId, UUID courseId){
-        String uri = "localhost:47112/grade/getGrade/" + studentId  + "/" + courseId ;
+    public Optional<Double> getGrade(UUID studentId, UUID courseId) {
+        String uri = "localhost:47112/grade/getGrade/" + studentId  + "/" + courseId;
         return restTemplate.getForObject(uri, Optional.class);
     }
 
-    /** Ask the Course microservice for the startDate corresponding to the course ID of the application
+    /** Ask the Course microservice for the startDate corresponding to
+     * the course ID of the application.
      *
      * @return An optional LocalDate
      */
-    public Optional<LocalDate> getCourseStartDate(UUID courseId){
+    public Optional<LocalDate> getCourseStartDate(UUID courseId) {
         String uri = "localhost:47112/course/getCourseStartDate/" + courseId;
         return restTemplate.getForObject(uri, Optional.class);
     }
 
-    /** check if the application is valid
+    /** Check if the application is valid.
      *
-     * @return true if valid, false if not
+     * @return true if valid, false if not.
      */
-    public static boolean validate(Application application){
+    public static boolean validate(Application application) {
         Boolean isValid = false;
-        try{
+        try {
             Validator validator = new IsCourseOpen(); // create chain of responsibility
             validator.setLast(new IsGradeSufficient());
             validator.setLast(new IsUniqueApplication());
 
             isValid = validator.handle(application);
-        } catch (InvalidApplicationException e){
+        } catch (InvalidApplicationException e) {
             e.printStackTrace();
         }
         return isValid;
