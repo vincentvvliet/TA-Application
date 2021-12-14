77560cb6546a37dc0fa1c4e4aaeaecd9d244d343
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2021-12-14 18:08:08 +0100

    Update security files

---
+++ User/src/main/java/nl/tudelft/sem/User/security/TokenAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package nl.tudelft.sem.User.security;
+
+import java.util.Optional;
+import lombok.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+
+@Component
+final class TokenAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+    @NonNull
+    UserAuthenticationService auth;
+
+    @Override
+    protected void additionalAuthenticationChecks(final UserDetails d, final UsernamePasswordAuthenticationToken auth) {
+        // Nothing to do
+    }
+
+    @Override
+    protected UserDetails retrieveUser(final String username, final UsernamePasswordAuthenticationToken authentication) {
+        final Object token = authentication.getCredentials();
+        return (UserDetails) Optional
+                .ofNullable(token)
+                .map(String::valueOf)
+                .flatMap(auth::findByToken)
+                .orElseThrow(() -> new UsernameNotFoundException("Cannot find user with authentication token=" + token));
+    }
+}
---
+++ User/src/main/java/nl/tudelft/sem/User/security/UserAuthenticationService.java
@@ -0,0 +1,33 @@
+package nl.tudelft.sem.User.security;
+
+
+import nl.tudelft.sem.User.entities.RealUser;
+
+import java.util.Optional;
+
+public interface UserAuthenticationService {
+
+    /**
+     * Logs in with the given {@code username} and {@code password}.
+     *
+     * @param username
+     * @param password
+     * @return an {@link Optional} of a user when login succeeds
+     */
+    Optional<String> login(String username, String password);
+
+    /**
+     * Finds a user by its dao-key.
+     *
+     * @param token user dao key
+     * @return
+     */
+    Optional<RealUser> findByToken(String token);
+
+    /**
+     * Logs out the given input {@code user}.
+     *
+     * @param user the user to logout
+     */
+    void logout(RealUser user);
+}
