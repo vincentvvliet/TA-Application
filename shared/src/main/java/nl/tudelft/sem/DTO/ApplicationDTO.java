4e3bb9e1010f650ef67be2f40d2bbd29912ca8fc
Gissa <G.vandeLinde@student.tudelft.nl>
2021-12-14 20:13:20 +0100

    Add getters and setters to DTOs

--- shared/src/main/java/nl/tudelft/sem/DTO/ApplicationDTO.java
+++ shared/src/main/java/nl/tudelft/sem/DTO/ApplicationDTO.java
@@ -2,16 +2,16 @@ package nl.tudelft.sem.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class ApplicationDTO {
-    UUID courseId;
-    UUID studentId;
-
+    @Getter @Setter UUID courseId;
+    @Getter @Setter UUID studentId;
 }
--- shared/src/main/java/nl/tudelft/sem/DTO/ApplyingStudentDTO.java
+++ shared/src/main/java/nl/tudelft/sem/DTO/ApplyingStudentDTO.java
@@ -2,16 +2,18 @@ package nl.tudelft.sem.DTO;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Optional;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class ApplyingStudentDTO {
-    private UUID studentId;
-    private double grade;
-    private Optional<Integer> rating;
+    @Getter @Setter private UUID studentId;
+    @Getter @Setter private double grade;
+    @Getter @Setter private Optional<Integer> rating;
 }
