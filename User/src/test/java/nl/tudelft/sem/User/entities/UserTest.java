59932b6fae90a6def36fa7af81091339ad30830f
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2021-12-17 02:41:34 +0100

    Add unit tests to UserTest

--- User/src/test/java/nl/tudelft/sem/User/entities/UserTest.java
+++ User/src/test/java/nl/tudelft/sem/User/entities/UserTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserTest {
 
+    /**
+     * The Empty user.
+     */
     User emptyUser;
+    /**
+     * The User.
+     */
     User user;
 
+    /**
+     * Setup to be run before each test.
+     */
     @BeforeEach
     public void setup() {
         emptyUser = new User();
@@ -18,10 +25,74 @@ class UserTest {
     }
 
     /**
-     * Test not null.
+     * Test not null of user with empty constructor.
+     */
+    @Test
+    public void testNotNullEmpty() {
+        Assertions.assertNotNull(emptyUser);
+    }
+
+    /**
+     * Test not null of user with normal constructor.
      */
     @Test
     public void testNotNull() {
-        Assertions.assertNotNull(emptyUser);
+        Assertions.assertNotNull(user);
+    }
+
+    /**
+     * Test get password of empty user.
+     */
+    @Test
+    public void testGetPasswordEmpty() {
+        Assertions.assertNull(emptyUser.getPassword());
+    }
+
+    /**
+     * Test get password.
+     */
+    @Test
+    public void testGetPassword() {
+        Assertions.assertEquals(user.getPassword(),"password");
+    }
+
+    /**
+     * Test get authorities.
+     */
+    @Test
+    public void testGetAuthorities() {
+        Assertions.assertTrue(user.isAccountNonExpired());
+    }
+
+    /**
+     * Test is account non expired.
+     */
+    @Test
+    public void testIsAccountNonExpired() {
+        Assertions.assertTrue(user.isAccountNonExpired());
+    }
+
+    /**
+     * Test is account non locked.
+     */
+    @Test
+    public void testIsAccountNonLocked() {
+        Assertions.assertTrue(user.isAccountNonExpired());
+    }
+
+    /**
+     * Test is credentials non expired.
+     */
+    @Test
+    public void testIsCredentialsNonExpired() {
+        Assertions.assertTrue(user.isAccountNonExpired());
+    }
+
+    /**
+     * Test is enabled.
+     */
+    @Test
+    public void testIsEnabled() {
+        Assertions.assertTrue(user.isAccountNonExpired());
     }
 }
