f3552f9a83cc9b4b43c7c2bb47af76172658e104
Andrei Tociu <A.C.Tociu@student.tudelft.nl>
2021-12-19 14:52:26 +0100

    add get-endpoint in Course controller and query in Course repo to return overlapping courses

--- Course/src/main/java/nl/tudelft/sem/Course/repositories/CourseRepository.java
+++ Course/src/main/java/nl/tudelft/sem/Course/repositories/CourseRepository.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.Course.repositories;
 
 import nl.tudelft.sem.Course.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,5 +12,12 @@ import java.util.UUID;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {
     List<Course> findByStartDateBetween(LocalDate currentDate, LocalDate selectionPeriod);
+
+    @Query(value = "SELECT c.id " +
+                   "FROM Course c " +
+                   "WHERE (c.startDate <= ?1 AND c.endDate >= ?1) " +  // course overlaps with start date
+                   "OR (c.startDate <= ?2 AND c.endDate >= ?2) " +     // course overlaps with end date
+                   "OR (c.startDate >= ?1 AND c.endDate <= ?2)")       // course in middle of specified course
+    List<UUID> findOverlappingCourses(LocalDate startDate, LocalDate endDate);
 
 }
