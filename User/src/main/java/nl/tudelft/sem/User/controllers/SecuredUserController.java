85ba70b57ba95cdb484ca5748f72cd8201223398
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2022-01-09 22:26:24 +0100

    Remove unnecessary methods from SecuredUUserController

--- User/src/main/java/nl/tudelft/sem/User/controllers/SecuredUserController.java
+++ User/src/main/java/nl/tudelft/sem/User/controllers/SecuredUserController.java
@@ -1,22 +1,21 @@
 package nl.tudelft.sem.User.controllers;
 
 import lombok.NonNull;
-import nl.tudelft.sem.User.entities.Role;
 import nl.tudelft.sem.User.entities.User;
 import nl.tudelft.sem.User.repositories.UserRepository;
 import nl.tudelft.sem.User.security.TokenAuthenticationService;
-import nl.tudelft.sem.User.security.UserAuthenticationService;
 import nl.tudelft.sem.User.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.List;
 
 /**
  * Secured controller only accessible by logged-in users.
@@ -65,37 +64,5 @@ public class SecuredUserController {
         System.out.println(user); //TODO user null
         authentication.logout(user);
         return true;
-    }
-
-    /**
-     * PATCH endpoint to accept a TA application.
-     *
-     * @param userId        of the lecturer who accepts the application
-     * @param applicationId of the application to be accepted
-     * @return true if the application was successfully accepted, false otherwise
-     */
-    @RequestMapping("/acceptApplication/{userId}/{applicationId}")
-    public Mono<Boolean> acceptApplication(@PathVariable(value = "userId") UUID userId, @PathVariable(value = "applicationId") UUID applicationId) throws Exception {
-        User user = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("user not found"));
-        if (user.getRole() != Role.LECTURER) {
-            throw new Exception("invalid role: only lecturers can accept applications");
-        }
-        return Mono.just(userService.acceptTaApplication(applicationId));
-    }
-
-    /**
-     * PATCH endpoint to accept a TA application.
-     *
-     * @param userId   of the user the application is of
-     * @param courseId the course the application is for
-     * @return true if the application was successfully created, false otherwise
-     */
-    @PostMapping("/createApplication/{userId}/{courseId}")
-    public Mono<Boolean> createApplication(@PathVariable(value = "userId") UUID userId, @PathVariable(value = "courseId") UUID courseId) throws Exception {
-        User user = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("user not found"));
-        if (user.getRole() != Role.STUDENT) {
-            throw new Exception("invalid role: only students can create applications");
-        }
-        return Mono.just(userService.createApplication(userId, courseId));
     }
 }
