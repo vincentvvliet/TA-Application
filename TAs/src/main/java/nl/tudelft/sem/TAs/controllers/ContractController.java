f0fd0ab0442280f59f410e8861a6be15d2c4cdab
Gissa <G.vandeLinde@student.tudelft.nl>
2022-01-15 11:56:12 +0100

    Fix pipline failure (update location of PortData in project structure)

--- TAs/src/main/java/nl/tudelft/sem/TAs/controllers/ContractController.java
+++ TAs/src/main/java/nl/tudelft/sem/TAs/controllers/ContractController.java
@@ -74,63 +74,6 @@ public class ContractController {
     }
 
     /**
-     * PATCH endpoint sets maxHours a TA can work on the contract, sends notification to user about updated contract.
-     * @param id of the contract to be updated
-     * @param  hours a TA can work
-     */
-    @PatchMapping("addHours/{id}/{maxHours}")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void addHoursById(@PathVariable (value = "id")  UUID id , @PathVariable(value = "maxHours") Integer hours) throws Exception {
-        Contract contract = contractRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        contract.setMaxHours(hours);
-        contractRepository.save(contract);
-        contractService.sendContractNotification(contract.getStudentId(), contract.getCourseId(), portData.getUserPort());
-    }
-
-    /**
-     * PATCH endpoint sets task description for contract, sends notification to user about updated contract.
-     * @param id of the contract
-     * @param task description
-     */
-    @PatchMapping("addTask/{id}")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void addTaskById(@PathVariable (value = "id")  UUID id, @RequestBody String task) throws Exception {
-        Contract contract = contractRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        contract.setTaskDescription(task);
-        contractRepository.save(contract);
-        contractService.sendContractNotification(contract.getStudentId(), contract.getCourseId(), portData.getUserPort());
-    }
-
-    /**
-     * PATCH endpoint sets the date when the contract starts, sends notification to user about updated contract.
-     * @param id of the contract.
-     * @param date representing the start time of the contract
-     */
-    @PatchMapping("addDate/{id}")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void addDateById(@PathVariable (value = "id")  UUID id, @RequestBody String date) throws Exception {
-        Contract contract = contractRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        Date d = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-        contract.setDate(d);
-        contractRepository.save(contract);
-        contractService.sendContractNotification(contract.getStudentId(), contract.getCourseId(), portData.getUserPort());
-    }
-
-    /**
-     * PATCH endpoint sets the salary the TA will get per hour
-     * @param id of the contract.
-     * @param salary (per hour) the TA will get
-     */
-    @PatchMapping("addSalary/{id}/{salary}")
-    @ResponseStatus(value = HttpStatus.OK)
-    public void addSalaryById(@PathVariable (value = "id")  UUID id, @PathVariable(value = "salary") double salary) throws Exception {
-        Contract contract = contractRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        contract.setSalaryPerHour(salary);
-        contractRepository.save(contract);
-        contractService.sendContractNotification(contract.getStudentId(), contract.getCourseId(), portData.getUserPort());
-    }
-
-    /**
      * DELETE endpoint deletes a contract by id
      * @param id of the contract
      * @return boolean representing if the deletion was successful or not
